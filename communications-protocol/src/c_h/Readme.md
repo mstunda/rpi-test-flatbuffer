86c62fe4582d6c9bc973e208a4bf3e0197994f19
Matiss Stunda <mstunda@gmail.com>
2021-03-25 09:49:22 +0200

    test file memory

---
+++ communications-protocol/src/c_h/Readme.md
@@ -0,0 +1,1 @@
+The particular directory is renamed from c to c_h so that another schema compilation would not overwrite it.
--- src/teleop-udp-server.c
+++ src/teleop-udp-server.c
@@ -7,54 +7,77 @@
         Paul Krzyzanowski
 */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <netdb.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include "../inc/teleop-udp-server.h"
+#include "teleop-udp-server.h"
 
 
 
-int fd;						// socket for udp
-//char buf[BUFSIZE];				// rx buffer
-struct sockaddr_in myaddr;				// local address
-struct sockaddr_in remaddr;				// remote address
-socklen_t addrlen = sizeof(remaddr);	// address length
-
-void teleop_udp_server_init()
+int teleop_udp_server_init()
 {
+	addrlen = sizeof(remaddr);
 	
 	// create a UDP socket
 	if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
 		perror("cannot create socket\n");
-		//return 0;
+		printf("cannot create socket\n");
+		return 0;
+		
 	}
 
-	// bind the socket to any valid IP address and a specific port
+	printf("socket fd in init: %d, addrlen: %d \n", fd, addrlen);
+
+
+	// bind the socket to any valid IP address and pick any port
 	memset((char *)&myaddr, 0, sizeof(myaddr));
 	myaddr.sin_family = AF_INET;
 	myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	myaddr.sin_port = htons(SERVICE_PORT);
+	myaddr.sin_port = htons(0);
+	
+	// ToDo: as server, switch to specific port
+	// bind the socket to any valid IP address and a specific port
+	//myaddr.sin_port = htons(SERVICE_PORT);
 
 	if (bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) {
 		perror("bind failed");
-	//	return 0;
+		printf("bind failed");
+		return 0;
 	}
+	
+	printf("socket fd at initialization: %d \n", fd);
+	
+	
+	return 1;
 	
 }
 
+
+int teleop_udp_client_local_init()
+{
+	char *server = "127.0.0.1";
+	memset((char *) &remaddr, 0, sizeof(remaddr));
+	
+	remaddr.sin_family = AF_INET;
+	remaddr.sin_port = htons(SERVICE_PORT);
+	if (inet_aton(server, &remaddr.sin_addr)==0) {
+		fprintf(stderr, "inet_aton() failed\n");
+		exit(1);
+	}
+	printf("remaddr successful \n");
+	return 1;
+}
+
+
+
+
 void teleop_udp_server_listen(char *p_buffer, int *p_recvlength)
 {
-		printf("waiting on port %d\n", SERVICE_PORT);
+		printf("waiting on port \n"); //ToDo - reset msg " %d\n", SERVICE_PORT);
 		*p_recvlength = recvfrom(fd, p_buffer, BUFSIZE, 0, (struct sockaddr *)&remaddr, &addrlen);
 		if (*p_recvlength > 0) { 
 			printf("rx ok!\n"); }
 		else printf("uh oh - something went wrong!\n");
 }
 
-void teleop_udp_server_respond(char *p_buffer, int length)
+void teleop_udp_server_report(char *p_buffer, int length)
 {
 		printf("sending response \"%s\"\n", p_buffer);
 		if (sendto(fd, p_buffer, length, 0, (struct sockaddr *)&remaddr, addrlen) < 0)
